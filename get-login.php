5e6d9944dbd234d78cc01961bea4834d1d9eeea3
abe <abeahaw@gmail.com>
2019-01-19 19:09:26 -0500

    Updated the Website (v2.0)
    Created actual home page and login.

---
+++ get-login.php
@@ -0,0 +1,41 @@
+<?php
+
+$username = 'root';
+$pass = '';
+$host = 'localhost';
+$dbname = 'spartahack_database';
+
+
+
+$dbc = @mysqli_connect($localhost, $username, $pass, $dbname) OR die('Could not connect to MySQL ' . mysqli_connect_error());
+	
+$query = "SELECT email, password FROM user_information";
+
+$response = @mysqli_query($dbc, $query);
+
+
+	$row = mysqli_fetch_array($response);
+	$user_email = $row["email"];
+	$user_pass = $row["password"];
+	
+	if($_POST["email"] == $user_email) {
+		if(	$_POST["password"] == $user_pass) {
+			echo "SUCCSESS";
+		} else {
+			echo "<p style='color:red; text-align:center; font-size: 20px; weight: bold;'>The username or password does not exist.</p>";
+			echo "<p style='color:red; text-align:center; font-size: 20px; weight: bold;'>Please enter a valid login</p>";
+			include "form.html";
+		}
+	} else {
+		
+		echo "<p style='color:red; text-align:center; font-size: 20px; weight: bold;'>The username or password does not exist.</p>";
+		echo "<p style='color:red; text-align:center; font-size: 20px; weight: bold;'>Please enter a valid login</p>";
+		include "form.html";
+	}
+				
+
+
+
+
+
+?>
